60d3612f828712b3546d9b71c145b443296eeb08
Laughter1470 <fidelisemma1470@gmail.com.com>
2025-10-10 16:38:00 +0100

    Initial frontend setup with student form

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://zjoagshtkasvxiveakdm.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb2Fnc2h0a2FzdnhpdmVha2RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MTMzNDAsImV4cCI6MjA3NTQ4OTM0MH0.kJ5zHxju3FsE-1eU7BeoLSQ_Gn6tDWSryZS90P2YC64';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpqb2Fnc2h0a2FzdnhpdmVha2RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MTMzNDAsImV4cCI6MjA3NTQ4OTM0MH0.kJ5zHxju3FsE-1eU7BeoLSQ_Gn6tDWSryZS90P2YC64';
 export const supabase = createClient(supabaseUrl, supabaseKey);
